6fdd49cf1229d0671a7b5652364b8eae5778f35f
Andhika Haeruman Santoso <Andhika Haeruman Santoso>
2016-11-17 10:17:04 +0700

    Penambahan fitur hitung skor, penghapusan fitur lihatskor dan penambahan lihat skor di main menu

--- main.cpp
+++ main.cpp
@@ -3,34 +3,37 @@
 
 using namespace std;
 void tambahPemain();
+void hitungSkor();
 void lihatSkor();
 string pemain[4];
 int skor[4]={0,0,0,0};
+int round = 0,skorTemp = 0;
 int main()
 {
     int pilih=0;
-    while(pilih != 6){
+    while(pilih != 5){
     system("cls");
     cout<<"===============================\n";
-    cout<<"Cekih Calculator by Andhika.H.S\n";
+    cout<<"Ceki  Calculator by Andhika.H.S\n";
     cout<<"===============================\n";
     cout<<"Main Menu:\n";
     cout<<"1. Tambah Pemain\n";
     cout<<"2. Hapus Pemain\n";
     cout<<"3. Hitung Skor\n";
-    cout<<"4. Lihat skor\n";
-    cout<<"5. Hall of Shame\n";
+    cout<<"4. Hall of Shame\n";
+    cout<<"5. Exit Program\n";
     cout<<"===============================\n";
+    lihatSkor();
     cout<<"Pilih: ";cin>>pilih;
     switch(pilih){
     case 1:{
         tambahPemain();
         break;
     }
-    case 4:{
-        lihatSkor();
+    case 3:{
+        hitungSkor();
         break;
-            }
+    }
         }
     }
 }
@@ -46,7 +49,6 @@ for(int i = 0;i<=3;i++){
 }
 
 void lihatSkor(){
-    system("cls");
     cout<<"===============================\n";
     cout<<"SKOR PERTANDINGAN TURNAMEN CEKI\n";
     cout<<"===============================\n";
@@ -54,5 +56,12 @@ void lihatSkor(){
     for(int i = 0;i<=3;i++){
         cout<< i+1 << ".\t" << pemain[i] << "\t\t\t" << skor[i] << "\n";
     }
-    system("pause");
+}
+
+void hitungSkor(){
+    system("cls");
+    for(int i = 0;i<=3;i++){
+        cout<<"Masukan Skor " << pemain[i] << " : ";cin>>skorTemp;
+        skor[i] = skor[i] + skorTemp;
+    }
 }
