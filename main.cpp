2ddc380fe305185c8f6ba55ad914838b9745ac0f
Andhika Haeruman Santoso <Andhika Haeruman Santoso>
2016-11-19 15:49:53 +0700

    Penghapusan fitur Hapus Pemain dan Penambahan Fitur Hall of Shame untuk pemain dengan skor terendah

--- main.cpp
+++ main.cpp
@@ -3,30 +3,33 @@
 #include <algorithm>
 
 using namespace std;
-void tambahPemain();
-void hitungSkor();
-void lihatSkor();
 struct players{
 int skor;
 string pemain;
 }player[4];
-/*string pemain[4];
-int skor[4]={0,0,0,0};*/
+
 int round = 0,skorTemp = 0;
+
+void tambahPemain();
+void hitungSkor();
+void lihatSkor();
+void resetSkor();
+void hallofShame();
 bool sort_skor(players const& lhs,players const&rhs);
+bool terendah(players const& lhs,players const&rhs);
 int main()
 {
     int pilih=0;
     while(pilih != 5){
     system("cls");
     cout<<"===============================\n";
-    cout<<"Ceki  Calculator by Andhika.H.S\n";
+    cout<<"CEKIH CALCULATOR BY ANDHIKA.H.S\n";
     cout<<"===============================\n";
     cout<<"Main Menu:\n";
     cout<<"1. Tambah Pemain\n";
-    cout<<"2. Hapus Pemain\n";
-    cout<<"3. Hitung Skor\n";
-    cout<<"4. Hall of Shame\n";
+    cout<<"2. Hitung Skor\n";
+    cout<<"3. Hall of Shame\n";
+    cout<<"4. Reset Skor\n";
     cout<<"5. Exit Program\n";
     cout<<"===============================\n";
     lihatSkor();
@@ -36,8 +39,16 @@ int main()
         tambahPemain();
         break;
     }
-    case 3:{
+    case 2:{
         hitungSkor();
+        break;
+    }
+    case 3:{
+        hallofShame();
+        break;
+    }
+    case 4:{
+        resetSkor();
         break;
     }
         }
@@ -46,9 +57,9 @@ int main()
 
 void tambahPemain(){
     system("cls");
-    cout<<"================================\n";
-    cout<<"King of the Iron Ceki Tournament\n";
-    cout<<"================================\n";
+    cout<<"=================================\n";
+    cout<<"KING OF THE IRON CEKIH TOURNAMENT\n";
+    cout<<"=================================\n";
 for(int i = 0;i<=3;i++){
     cout<<"Nama Pemain " << i+1 << " : ";cin>>player[i].pemain;
 }
@@ -56,9 +67,9 @@ for(int i = 0;i<=3;i++){
 
 void lihatSkor(){
     std::sort(player, player+4, &sort_skor);
-    cout<<"===============================\n";
-    cout<<"SKOR PERTANDINGAN TURNAMEN CEKI\n";
-    cout<<"===============================\n";
+    cout<<"================================\n";
+    cout<<"SKOR PERTANDINGAN TURNAMEN CEKIH\n";
+    cout<<"================================\n";
     cout<<"No.\t" << "Nama Pemain\t\t" << "Skor\n";
     for(int i = 0;i<=3;i++){
         cout<< i+1 << ".\t" << player[i].pemain << "\t\t\t" << player[i].skor << "\n";
@@ -73,7 +84,29 @@ void hitungSkor(){
     }
 }
 
+void resetSkor(){
+    for(int i=0;i<=3;i++){
+        player[i].skor=0;
+    }
+}
+
+void hallofShame(){
+    system("cls");
+    std::sort(player, player+4, &terendah);
+    cout<<"=============\n";
+    cout<<"HALL OF SHAME\n";
+    cout<<"=============\n";
+    cout<< player[0].pemain <<" Adalah Pemain Tercupu Sepanjang Sejarah Dengan Skor " << player[0].skor << "\n";
+
+    system("pause");
+}
+
 bool sort_skor(players const& lhs,players const&rhs){
     if(lhs.skor != rhs.skor)
         return lhs.skor > rhs.skor;
+}
+
+bool terendah(players const& lhs,players const&rhs){
+    if(lhs.skor != rhs.skor)
+            return lhs.skor < rhs.skor;
 }
