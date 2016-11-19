36ede60ea6671736390d4aa8ba01d18cc0af2e59
Andhika Haeruman Santoso <Andhika Haeruman Santoso>
2016-11-19 15:01:06 +0700

    Penambahan Fitur Sorting Berdasarkan Skor Pada Papan Skor

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <stdlib.h>
+#include <algorithm>
 
 using namespace std;
 void tambahPemain();
 void hitungSkor();
 void lihatSkor();
-string pemain[4];
-int skor[4]={0,0,0,0};
+struct players{
+int skor;
+string pemain;
+}player[4];
+/*string pemain[4];
+int skor[4]={0,0,0,0};*/
 int round = 0,skorTemp = 0;
+bool sort_skor(players const& lhs,players const&rhs);
 int main()
 {
     int pilih=0;
@@ -44,24 +50,30 @@ void tambahPemain(){
     cout<<"King of the Iron Ceki Tournament\n";
     cout<<"================================\n";
 for(int i = 0;i<=3;i++){
-    cout<<"Nama Pemain " << i+1 << " : ";cin>>pemain[i];
+    cout<<"Nama Pemain " << i+1 << " : ";cin>>player[i].pemain;
 }
 }
 
 void lihatSkor(){
+    std::sort(player, player+4, &sort_skor);
     cout<<"===============================\n";
     cout<<"SKOR PERTANDINGAN TURNAMEN CEKI\n";
     cout<<"===============================\n";
     cout<<"No.\t" << "Nama Pemain\t\t" << "Skor\n";
     for(int i = 0;i<=3;i++){
-        cout<< i+1 << ".\t" << pemain[i] << "\t\t\t" << skor[i] << "\n";
+        cout<< i+1 << ".\t" << player[i].pemain << "\t\t\t" << player[i].skor << "\n";
     }
 }
 
 void hitungSkor(){
     system("cls");
     for(int i = 0;i<=3;i++){
-        cout<<"Masukan Skor " << pemain[i] << " : ";cin>>skorTemp;
-        skor[i] = skor[i] + skorTemp;
+        cout<<"Masukan Skor " << player[i].pemain << " : ";cin>>skorTemp;
+        player[i].skor = player[i].skor + skorTemp;
     }
+}
+
+bool sort_skor(players const& lhs,players const&rhs){
+    if(lhs.skor != rhs.skor)
+        return lhs.skor > rhs.skor;
 }
